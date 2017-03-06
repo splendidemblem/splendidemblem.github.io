88fef4df9f758cb28394db3e344c22b3afe16816
violetlight <jasonhfields@gmail.com>
2017-03-06 17:20:45 -0600

    Big title, auto-determine letter-spacing property

--- js/client.js
+++ js/client.js
@@ -1,1 +1,31 @@
-console.log('hello world');
+$.fn.stretchText = function() {
+  var el              = $(this),
+      containerWidth  = el.width(),
+      txt             = el.html(),
+      projectedLine   = $('<span class="stretched">'+txt+'</span>'),
+      totalCharsInTxt = el.text().length,
+      spacing         = containerWidth / totalCharsInTxt,
+      txtWidth;
+
+      el.html(projectedLine);
+      txtWidth = projectedLine.width();
+
+      if (txtWidth < containerWidth) {
+        var charWidth = txtWidth / totalCharsInTxt,
+            letterSpacing = spacing - charWidth + (spacing - charWidth) / totalCharsInTxt;
+
+        console.log(letterSpacing);
+        projectedLine.css({'letter-spacing': letterSpacing});
+      } else {
+        projectedLine.contents().unwrap();
+        el.addClass('justify');
+      }
+};
+
+
+$(document).ready(function() {
+  $('.stretch-me').each(function() {
+    $(this).stretchText();
+  });
+});
+
